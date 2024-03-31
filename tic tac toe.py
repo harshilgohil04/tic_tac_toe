970e53ed5ecc3e716007b3871a52b31951db81cf
harshilgohil04 <harshilgohil4u24@gmail.comgit config --global user.name>
2024-03-30 23:05:56 -0700

    secound commit

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,19 +1,36 @@
-board= ["-", "-", "-",
-        "_", "-", "-",
-        "-", "-", "-"]
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+currentplayer = "X"
 
-print(board[0] + " | " + board[1] + " | " + board[2])
-print("-----------")
-print(board[3] + " | " + board[4] + " | " + board[5])
-print("-----------")
-print(board[6] + " | " + board[7] + " | " + board[8])
+def print_board():
+    print(board[0] + "|", board[1] + "|", board[2])
+    print("-------")
+    print(board[3] + "|", board[4] + "|", board[5])
+    print("-------")
+    print(board[6] + "|", board[7] + "|", board[8])
 
-inp=int(input(" enter a number 1 to 9: "))
-if inp >= 1 and inp<=9 and board[inp-1] =="-":
-    board[inp -1 ]= "x"
-else:
-    " already taken "
+print_board()
 
-    while gamerunning:
-        print(board)
-        print (inp)
+while True:
+    inp = int(input("Enter a number between 1-9: "))
+    if inp <= 9 and inp >= 1 and board[inp - 1] == "-":
+        board[inp - 1] = currentplayer
+        print_board()
+        # Check for winning condition
+        # Check rows
+        if (board[0] == board[1] == board[2] != "-") or \
+           (board[3] == board[4] == board[5] != "-") or \
+           (board[6] == board[7] == board[8] != "-") or \
+           (board[0] == board[3] == board[6] != "-") or \
+           (board[1] == board[4] == board[7] != "-") or \
+           (board[2] == board[5] == board[8] != "-") or \
+           (board[0] == board[4] == board[8] != "-") or \
+           (board[2] == board[4] == board[6] != "-") :
+
+                print("Player", currentplayer, "wins!")
+        
+        # Switch player
+        currentplayer = "O" if currentplayer == "X" else "X"
+    else:
+        print("Invalid input or cell already taken. Please try again.")
